ba25fe5755c4cd17965b7f3cbb78354a811b0302
ebuberoderick <ebuberoderick2@gmail.com>
2024-11-11 14:58:01 +0000

    created the first screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,13 +5,19 @@ import PagerView from 'react-native-pager-view';
 const MyPager = () => {
   return (
     <PagerView style={styles.pagerView} initialPage={0}>
-      <View className='flex-1 justify-center items-center bg-red-500' key="1">
-        <Text>First page</Text>
+      <View className='flex-1 justify-center items-center bg-white' key="1">
+        <View className='gap-5'>
+          <View className='h-96 w-96 rounded-full bg-slate-200' />
+          <Text className='font-semibold text-3xl text-center'>Welcome to Surf</Text>
+          <Text className='text-gray-400 text-center max-w-sm'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus natus nihil nisi impedit fugit esse rerum maiores 
+          </Text>
+        </View>
       </View>
-      <View className='flex-1 justify-center items-center bg-green-500' key="2">
+      <View className='flex-1 justify-center items-center bg-white' key="2">
         <Text>Second page</Text>
       </View>
-      <View className='flex-1 justify-center items-center bg-pink-500' key="3">
+      <View className='flex-1 justify-center items-center bg-white' key="3">
         <Text>Thrid page</Text>
       </View>
     </PagerView>
