7f8c713bfa6c1c04fd095e1a10a6e250151b5406
ebuberoderick <ebuberoderick2@gmail.com>
2024-11-11 15:04:15 +0000

    created all screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,20 +5,32 @@ import PagerView from 'react-native-pager-view';
 const MyPager = () => {
   return (
     <PagerView style={styles.pagerView} initialPage={0}>
-      <View className='flex-1 justify-center items-center bg-white' key="1">
+      <View className='flex-1 justify-center items-center dark:bg-black bg-white' key="1">
         <View className='gap-5'>
-          <View className='h-96 w-96 rounded-full bg-slate-200' />
-          <Text className='font-semibold text-3xl text-center'>Welcome to Surf</Text>
+          <View className='h-96 w-96 rounded-full bg-slate-100 dark:bg-slate-700' />
+          <Text className='font-semibold text-3xl text-center dark:text-white'>Welcome to Surf</Text>
           <Text className='text-gray-400 text-center max-w-sm'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus natus nihil nisi impedit fugit esse rerum maiores 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
           </Text>
         </View>
       </View>
-      <View className='flex-1 justify-center items-center bg-white' key="2">
-        <Text>Second page</Text>
+      <View className='flex-1 justify-center items-center dark:bg-black bg-white' key="2">
+        <View className='gap-5'>
+          <View className='h-96 w-96 mx-auto rounded-full bg-slate-100 dark:bg-slate-700' />
+          <Text className='font-semibold text-3xl text-center dark:text-white max-w-md'>Design Template uploads Every Tuesday</Text>
+          <Text className='text-gray-400 text-center max-w-sm mx-auto'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </Text>
+        </View>
       </View>
-      <View className='flex-1 justify-center items-center bg-white' key="3">
-        <Text>Thrid page</Text>
+      <View className='flex-1 justify-center items-center dark:bg-black bg-white' key="3">
+        <View className='gap-5'>
+          <View className='h-96 w-96 mx-auto rounded-full bg-slate-100 dark:bg-slate-700' />
+          <Text className='font-semibold text-3xl text-center dark:text-white max-w-md'>Download now!</Text>
+          <Text className='text-gray-400 text-center max-w-sm mx-auto'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </Text>
+        </View>
       </View>
     </PagerView>
   );
